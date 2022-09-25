e584b40a10a3296f641b61785a8a51fc01da9d66
Damian Boh <bck1990@gmail.com>
2022-09-25 20:42:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -435,8 +435,4 @@ def download_file(filename):
     return send_file(filename, as_attachment=True, cache_timeout=0)
 
 if __name__ == '__main__':
-    app.run(
-        host="0.0.0.0",
-        port=int("5000"),
-        debug=True
-    )
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
